8ccba4592c2374dac9f9ce7cddb18c01310f47ca
Ethan <ehwehaws@gmail.com>
2025-09-14 01:42:57 -0600

    small start

---
+++ week02/Journal/entry.cs
@@ -0,0 +1,13 @@
+//date, prompt, respone, and display will be kept in this file
+
+public class entry
+{
+    public string _date;
+    public string _prompt;
+    public string _respone;
+
+    public void Display()
+    {
+        
+    }
+}
