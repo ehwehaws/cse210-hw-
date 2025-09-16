b45d4ec98a862e53e81e106adfeb296aebf7e75b
Ethan <ehwehaws@gmail.com>
2025-09-15 20:54:01 -0600

    finsihed the program.cs along with the other files and testing

--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -21,7 +21,36 @@ public class Journal
             entry.Display();
         }
     }
-    
+    //savespotincasedoesntwork
+    public void SaveToFile(string filename)
+    {
+        using (StreamWriter sw = new StreamWriter(filename))
+        {
+            foreach (Entry entry in _entries)
+            {
+                sw.WriteLine($"{entry._date}|{entry._prompt}|{entry._response}");
+            }
+        }
+    }
+
+
+    public void LoadFromFile(string filename)
+    {
+        _entries.Clear();
+
+        string[] lines = File.ReadAllLines(filename);
+
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split("|");
+
+            Entry entry = new Entry();
+            entry._date = parts[0];
+            entry._prompt = parts[1];
+            entry._response = parts[2];
+            _entries.Add(entry);
+        }
+    }
 
 
 
--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -10,7 +10,7 @@ class Program
 
         Journal journal = new Journal();
         Random rnd = new Random();
-        
+
         List<string> prompts = new List<string>()
         {
             "What was your favorite activity today?",
@@ -20,18 +20,46 @@ class Program
             "Who was the first person you talked to today?"
         };
 
+        bool running = true;
 
+        while (running)
+        {
+            Console.WriteLine("1. Write new entry");
+            Console.WriteLine("2. Display journal");
+            Console.WriteLine("3. Save journal");
+            Console.WriteLine("4. Load journal");
+            Console.WriteLine("5. Quit");
+            Console.WriteLine("Write choice here: ");
+            string choice = Console.ReadLine();
 
+            if (choice == "1")
+            {
+                string prompt = prompts[rnd.Next(prompts.Count)];
+                Console.WriteLine(prompt);
+                string response = Console.ReadLine();
+                journal.AddEntry(prompt, response);
+            }
+            else if (choice == "2")
+            {
+                journal.DisplayJournal();
+            }
+            else if (choice == "3")
+            {
+                Console.Write("What is the filename? :");
+                string filename = Console.ReadLine();
+                journal.SaveToFile(filename);
+            }
+            else if (choice == "4")
+            {
+                Console.Write("Enter filename: ");
+                string filename = Console.ReadLine();
+                journal.LoadFromFile(filename);
+            }
+            else if (choice == "5")
+            {
+                running = false;
+            }
 
-
-
-
-
-
-
-
-
-
-
+        }
     }
 }
