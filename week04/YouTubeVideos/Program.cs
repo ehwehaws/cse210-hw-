bb4892df235e3e0dafd22c49798c5ffdb4bd98ef
Ethan <ehwehaws@gmail.com>
2025-09-29 00:22:29 -0600

    added comments

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -10,12 +10,15 @@ class Program
 
         v1.AddComment(new Comment("ProGamer1", "Haha lol"));
         v1.AddComment(new Comment("SmartGuy101", "Too funny!"));
+        v1.AddComment(new Comment("NoFunnyBusinessMan", "Not funny, didnt laugh"));
 
         v2.AddComment(new Comment("SmartCodingDude", "Too easy"));
         v2.AddComment(new Comment("John", "Can you go a little more in depth next time?"));
+        v2.AddComment(new Comment("Jonathieghn", "My head hurts"));
 
         v3.AddComment(new Comment("DumbDude", "Ok"));
         v3.AddComment(new Comment("IDontGotTimeForThisMan2295", "Thank you for keeping it short"));
+        v3.AddComment(new Comment("OkImLazy6534", "Great now i know how to code"));
 
         List<YouVid> vids = new List<YouVid>() { v1, v2, v3 };
 
