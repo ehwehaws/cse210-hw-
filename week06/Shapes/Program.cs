6f870d0bbd643962d8a083b883604b0cd79c39d2
ehwehaws <ehwehaws@gmail.com>
2025-10-16 03:17:37 -0600

    test12

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        List<Shape> shapes = new List<Shape>();
+        List<Shape> shapes = new List<Shape>(); test12
         
     }
 }
